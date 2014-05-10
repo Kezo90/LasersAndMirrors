13eda414a910f7741f285f6eb7411146ff384461
kezo90 <kerekeszoltan90@freemail.hu>
2014-05-10 21:45:41 +0200

    unneeded comment

--- src/main/java/hu/unideb/inf/lasersandmirrors/InputHandler.java
+++ src/main/java/hu/unideb/inf/lasersandmirrors/InputHandler.java
@@ -1,7 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hu.unideb.inf.lasersandmirrors;
 
 import hu.unideb.inf.lasersandmirrors.gameobject.GameObject;
