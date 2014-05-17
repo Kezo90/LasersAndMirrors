e922defb0a160f5a97556230eb1db8a08d215103
kezo90 <kerekeszoltan90@freemail.hu>
2014-05-17 17:40:26 +0200

    First test.

---
+++ src/test/java/hu/unideb/inf/lasersandmirrors/gameobject/GameObjectTest.java
@@ -0,0 +1,32 @@
+
+package hu.unideb.inf.lasersandmirrors.gameobject;
+
+import java.util.List;
+import org.junit.*;
+
+/**
+ *
+ * @author KZ
+ */
+public class GameObjectTest extends TestCase {
+	
+	public GameObjectTest(String testName) {
+		super(testName);
+	}
+	
+	@Before
+	public void init(){
+		
+	}
+
+	/**
+	 * Test of sortGameObjectsByDepth method, of class GameObject.
+	 */
+	public void testSortGameObjectsByDepth() {
+		System.out.println("sortGameObjectsByDepth");
+		List<GameObject> gameObjects = null;
+		GameObject.sortGameObjectsByDepth(gameObjects);
+		// TODO review the generated test code and remove the default call to fail.
+		fail("The test case is a prototype.");
+	}
+}
